59d672ed24ccacacd79b90ecdc9a4ec9d6bdb362
Muhammad Shayan Faisal <muhammadshayanfaisal@gmail.com>
2024-06-14 18:41:28 +0500

    100Days Coding Challenge

---
+++ Day54/variables.js
@@ -0,0 +1,15 @@
+var number = 3;
+var user = 'John';
+var age = 25;
+var message = 'Hello';
+//let 1a; // cannot start with a digit
+//let my-name; // hyphens '-' aren't allowed in the name
+// let let = 5; // can't name a variable "let", error!
+// let return = 5; // also can't name it "return", error!
+var COLOR_RED = "#F00";
+var COLOR_GREEN = "#0F0";
+var COLOR_BLUE = "#00F";
+var COLOR_ORANGE = "#FF7F00";
+// ...when we need to pick a color
+var color = COLOR_ORANGE;
+console.log(color); // #FF7F00
---
+++ Day54/variables.ts
@@ -0,0 +1,15 @@
+let number:number=3;
+let user = 'John';
+let age = 25;
+let message = 'Hello';
+//let 1a; // cannot start with a digit
+//let my-name; // hyphens '-' aren't allowed in the name
+// let let = 5; // can't name a variable "let", error!
+// let return = 5; // also can't name it "return", error!
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+console.log(color); // #FF7F00
