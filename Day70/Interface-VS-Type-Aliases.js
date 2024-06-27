61e30ea8e8ff51e092ede7f4cb4acb7f7525e1d2
Muhammad Shayan Faisal <muhammadshayanfaisal@gmail.com>
2024-06-27 17:26:52 +0500

    100Days of Coding Challenge

---
+++ Day70/Interface-VS-Type-Aliases.js
@@ -0,0 +1,19 @@
+var Labrador = /** @class */ (function () {
+    function Labrador() {
+        this.type = "dog";
+    }
+    Labrador.prototype.bark = function () {
+        console.log("Woof!");
+    };
+    return Labrador;
+}());
+var labrador = {
+    type: "monkey",
+    bark: function () {
+        console.log("Woof!");
+    },
+};
+var myCar = {
+    brand: "Toyota",
+    model: "Camry",
+};
