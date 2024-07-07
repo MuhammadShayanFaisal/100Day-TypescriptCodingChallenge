30f23f12f7421a330379219893b366aa3e1f0907
Muhammad Shayan Faisal <muhammadshayanfaisal@gmail.com>
2024-07-08 00:22:28 +0500

    100Daays Coding Challenge

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -1,3 +1,10 @@
+// Fetch the last 5 invoices, sorted by date
+const data = await sql<LatestInvoiceRaw>`
+  SELECT invoices.amount, customers.name, customers.image_url, customers.email
+  FROM invoices
+  JOIN customers ON invoices.customer_id = customers.id
+  ORDER BY invoices.date DESC
+  LIMIT 5`;
 import { sql } from '@vercel/postgres';
 import {
   CustomerField,
