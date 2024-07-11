8a03fec2c733487b236bcab27ae27a2dcfadf815
Muhammad Shayan Faisal <muhammadshayanfaisal@gmail.com>
2024-07-12 00:25:57 +0500

    100Days Coding Challenge

--- nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -18,8 +18,11 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  return (
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+ 
+  return (<form action={formAction}></form>;
     <form action={updateInvoiceWithId}></form>
     <input type="hidden" name="id" value={invoice.id} />
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
