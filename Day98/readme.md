09b1e59bd967d98ac3b1e8a3ebab6c40dbc0d09f
Muhammad Shayan Faisal <muhammadshayanfaisal@gmail.com>
2024-07-12 15:20:32 +0500

    100Days Coding Challenge

---
+++ Day98/readme.md
@@ -0,0 +1,2 @@
+I have read and understand the following article
+[Adding Authentication](https://nextjs.org/learn/dashboard-app/adding-authentication)
--- nextjs-dashboard/app/ui/dashboard/sidenav.tsx
+++ nextjs-dashboard/app/ui/dashboard/sidenav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
-
+import { signOut } from '@/auth';
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
@@ -18,6 +18,11 @@ export default function SideNav() {
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form>
+        action={async () => {
+            'use server';
+            await signOut();
+          }}
+        >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
